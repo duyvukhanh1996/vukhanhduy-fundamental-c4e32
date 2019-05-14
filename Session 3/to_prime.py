61af784138337183e388dde51918da181539592b
KhanhDuy <duyvukhanh113@gmail.com>
2019-05-14 18:41:34 +0700

    hihi

--- Session 3/to_prime.py
+++ Session 3/to_prime.py
@@ -1,16 +1,14 @@
-list_prime = []
-
 n = int(input("Nhap N:"))
+while not 0 < n < 1000000:
+    n = int(input("Nhap lai N > 0 va N < 10000:"))
+
 a = n
-i = 2
-
-while a != 1:  
-    if a % i == 0:
-        list_prime.append(i)
-        a = a/i
-    else:
-        i += 1
-
-   
+list_prime = []
+while a != 1:
+    for i in range(2,n):  #????
+        if a % i == 0:
+            list_prime.append(i)
+            a = a/i
+            break
 print(n,"=",end=" ")
 print(*list_prime,sep="*")
