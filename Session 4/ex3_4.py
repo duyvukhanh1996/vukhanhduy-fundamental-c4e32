2afd3ce760c7b1981d8925aa9a52c151b981f15c
KhanhDuy <duyvukhanh113@gmail.com>
2019-05-18 14:07:24 +0700

    'fix'

--- Session 4/ex3_4.py
+++ Session 4/ex3_4.py
@@ -1,17 +1,20 @@
 Questions = [
     {
+    "Intro" : "Answer the following algebra question:",
     "Question" : "If x = 8, then what is the value of 4(x+3) ?",
     "Answer" : [35,36,40,44],
     "Correct" : "4"
     },
     {
-    "Question" : "Jack scored these marks in 5 math tests: 49, 81, 72, 66 and 52. What is the mean? ?",
+    "Intro" : "Estimate this answer (exact calculation not needed):",
+    "Question" : "Jack scored these marks in 5 math tests: 49, 81, 72, 66 and 52. What is the mean?",
     "Answer" : ["About 55","About 65","About 75","About 85"],
     "Correct" : "2"
     }
 ]
 correctly_answer = 0
 for question in Questions:
+    print(question["Intro"])
     print(question["Question"])
     for i in question["Answer"]:
         print(question["Answer"].index(i)+1,". ",i)
